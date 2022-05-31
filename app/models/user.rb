37adb95fe23ee50cdb938e35de5a5a42b30a2dbf
Thanh Le <thanhlevan0203@gmail.com>
2022-05-30 23:57:50 -0700

    Handled Sign Up Form Errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@
 # password_confirmation:string virtual
 
 class User < ApplicationRecord
-  #has_secure_password
+  has_secure_password
 
-  #validates : email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
 end
