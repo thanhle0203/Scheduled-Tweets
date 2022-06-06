f3a2b542abbbbb0c154716459f9eec840a98e87a
Thanh Le <thanhlevan0203@gmail.com>
2022-06-05 23:57:26 -0700

    Created Login Form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_27_083843) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_054239) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
