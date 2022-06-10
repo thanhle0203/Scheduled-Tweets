44d823a408d6d50fc0564209a0023b2c3ae2b7f7
Thanh Le <thanhlevan0203@gmail.com>
2022-06-09 19:21:51 -0700

    Created the Reset Password Token Mailer

---
+++ app/mailers/password_mailer.rb
@@ -0,0 +1,13 @@
+class PasswordMailer < ApplicationMailer
+ # Subject can set in your I18n file at config/locales.en.yml
+ # with the following lookup:
+ #
+ # en.password_mailer.reset.subject
+ #
+  def reset
+    params[:user]
+
+    mail to: params[:user].email
+  end
+
+end
